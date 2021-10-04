879d653f54f8a691411580e8027f8edba8a29c27
MohammedHassan25 <87442047+MohammedHassan25@users.noreply.github.com>
2021-10-05 00:47:12 +0200

    chap 05 : Query a Database
    chap 05 : Query a Database

---
+++ chap 05/01_Creating a database.py
@@ -0,0 +1,4 @@
+# -----------------------------------------------------------------------------------------
+# When we create a database or modify its schema, we'll use SQL as a DDL,
+# and when we work with data inside the structure of a database, we'll use SQL as a DML.
+# -----------------------------------------------------------------------------------------
---
+++ chap 05/02_Query a Database.py
@@ -0,0 +1,15 @@
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Depending on the DBMS you're using, the way you set up and enforce this relationship will be different.
+# Once a schema is in place we need to use SQL as a data manipulation language to work with the data itself.
+# A query returns specific information that we ask for.
+# From a database we'll use the select statement. This tells the database we want to see information from specific fields.
+# The select clause allows us to return data from the tables in our database and it acts as a starting point for making more complex and detailed queries as well.
+# We can use the WHERE clause to choose only specific records or records which match our criteria.
+# The percent is the placeholder for one character.
+# The count function takes the name of a field and tells us how many rows that match our criteria have a value in that field.
+# (Sum) adds up all the values of the matching criteria
+# We can also get an average of values with the AVG keyword.
+# We can get the minimum and maximum values in a column with min and max.
+# We can use the JOIN keyword to tell the database to consider two tables.
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+

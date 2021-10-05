9523e0557d664f56295ac1d79348840b2d4c9128
MohammedHassan25 <87442047+MohammedHassan25@users.noreply.github.com>
2021-10-05 05:30:19 +0200

    Add files via upload

---
+++ chap 05/03_Indexes, transactions, and stored procedures.py
@@ -0,0 +1,7 @@
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Just like in a book, tables in a database can have indexes, where you can look up information quickly.
+# Transactions group queries or statements into a block of activities, where, if one of the components fails for any reason, the whole group of statements is not executed, and anything that's partially done is rolled back.
+# A stored procedure is kind of like a program you write that's stored on the database server.
+# A stored procedure contains a series of commands that you can then reference and use when you interact with the database.
+# Stored procedures are also used to provide safe or approved ways of dealing with sensitive data.
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
---
+++ chap 05/04_Access control, compliance, and injection.py
@@ -0,0 +1,6 @@
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# In the database, we can grant users varying levels of access to hold databases, tables, and even specific actions.
+# When you're designing a database and granting access to the database and the data it's important to consider your business requirements.
+# It's also very important to consider any compliance rules you need to follow.
+# An attacker could try to enter a value that is part of an SQL command to hijack the query we think we're running and change how it works. This is called SQL injection
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
